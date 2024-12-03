96ab3fc1fde088783844bdfc5b1717280d7cb9e6
Ciallo <dison2233@126.com>
2024-12-03 11:48:11 +0800

    hltv break my plugin

--- src/movement/mv_hooks.cpp
+++ src/movement/mv_hooks.cpp
@@ -208,7 +208,7 @@ static void Hook_OnProcessMovement(CCSPlayer_MovementServices* ms, CMoveData* mv
 
 static void Hook_OnPhysicsSimulate(CCSPlayerController* pController) {
 	if (!pController || pController->m_bIsHLTV()) {
-		return MEM::SDKCall<void>(MOVEMENT::TRAMPOLINE::g_fnPhysicsSimulate, pController);
+		return;
 	}
 
 	SurfPlugin()->simulatingPhysics = true;
